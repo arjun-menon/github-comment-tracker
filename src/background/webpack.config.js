bb5d2904645cf44744f714447f0197d063907836
Arjun G. Menon <contact@arjungmenon.com>
2017-06-30 01:01:31 -0400

    Create stub background script

---
+++ src/background/webpack.config.js
@@ -0,0 +1,9 @@
+var path = require('path');
+
+module.exports = {
+  entry: './ghct-background.js',
+  output: {
+    filename: 'ghct-background.js',
+    path: path.resolve('../..', 'dist')
+  }
+};
